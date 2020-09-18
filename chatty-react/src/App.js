28ca310afc1b9dcf0bfdc6d87244ace494d7c1c1
Bradley Wilson <bluedirtbike6@gmail.com>
2020-09-18 17:30:49 -0500

    create buttons for routing to join and create page, add jumbotron for style.

--- chatty-react/src/App.js
+++ chatty-react/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
 import Home from './components/pages/Home';
 import Join from './components/pages/Join';
 import Create from './components/pages/Create';
@@ -10,12 +9,10 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Container>
       <Route exact path="/" component={Home} />
       <Route path="/Join" component={Join} />
       <Route path="/create" component={Create} />
       <Route path="/Chat" component={Chat} />
-      </Container>
     </Router>
   ); 
 }
--- chatty-react/src/components/pages/Home.js
+++ chatty-react/src/components/pages/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import Nav from '../sub-components/Nav';
+import React from "react";
+import Nav from "../sub-components/Nav";
+import { Button, Container, Jumbotron } from "react-bootstrap";
 const Home = () => {
-    return (
-        <div>
-            <Nav />
-        </div>
-    );
+  return (
+    <div>
+      <Nav />
+      <br></br>
+      <br></br>
+      <Container>
+        <Jumbotron fluid>
+          <Container>
+            <h1 className="text-center">Welcome</h1>
+            <p className="text-center">
+              Chatty is a bi-directional messaging application that utilizes WebSockets.
+            </p>
+          </Container>
+        </Jumbotron>
+        <br></br>
+        <Button variant="primary" size="lg" block href="/join">
+          Join Room
+        </Button>
+        <Button variant="danger" size="lg" block href="/create">
+          Create Room
+        </Button>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
