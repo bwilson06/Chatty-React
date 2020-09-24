702ac79886e9ae9ce45d7be5c3bff21457c3e8ad
Bradley Wilson <bluedirtbike6@gmail.com>
2020-09-24 00:36:20 -0500

    add styling to chat page.

--- chatty-react/src/components/pages/Chat.js
+++ chatty-react/src/components/pages/Chat.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import Nav from "../sub-components/Nav"
-
+import React, { Component } from "react";
+import { Container, Button, InputGroup, FormControl } from "react-bootstrap";
+import Nav from "../sub-components/Nav";
+import "../Main.css";
 class Chat extends Component {
-    componentDidMount(){
-        let id = this.props.match.params.room_id
-        console.log(id)
-    }
-    render() {
-        return (
-            <div>
-               <Nav />
-            </div>
-        );
-    }
+  componentDidMount() {
+    let id = this.props.match.params.room_id;
+    console.log(id);
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Container>
+          <div className="chatbox">
+            <InputGroup>
+              <FormControl
+                placeholder="Enter your message"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-primary">Send</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Chat;
