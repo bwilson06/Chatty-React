8211b31411c75d811ea58624a34b387af34e0483
Bradley Wilson <bluedirtbike6@gmail.com>
2020-09-29 01:12:15 -0500

    implement react-scroll.

--- chatty-react/src/components/pages/Chat.js
+++ chatty-react/src/components/pages/Chat.js
@@ -5,71 +5,86 @@ import ChatContainer from "../sub-components/ChatContainer";
 import "../Main.css";
 import axios from "axios";
 import io from "socket.io-client";
+import { animateScroll } from "react-scroll";
 
 class Chat extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-    message: "",
-    chats: [],
-  };
+      message: "",
+      chats: [],
+    };
 
-  this.socket = io.connect('http://localhost:3001');
+    this.socket = io.connect("http://localhost:3001");
 
-  this.socket.on('message', (message) => {
-    if (this.state.chats.length > 0){
-    this.setState({ chats: [...this.state.chats, message]} )
-    }
-  })
+    this.socket.on("message", (message) => {
+      if (this.state.chats.length > 0) {
+        this.setState({ chats: [...this.state.chats, message] });
+        this.scrollToBottom();
+      }
+    });
 
-  this.handleChange = (event) => {
-    event.preventDefault();
-    this.setState({ message: event.target.value });
-  };
+    this.scrollToBottom = () => {
+      animateScroll.scrollToBottom({
+        containerId: "chatbox",
+        duration: 500,
+      });
+    };
 
-  this.handleSubmit = (event) => {
-         event.preventDefault();
-         let newChat = {
-           username: this.props.match.params.username,
-           message: this.state.message
-         }
-         this.socket.emit('message', newChat)
-         var chat = {
-           username: this.props.match.params.username,
-           message: this.state.message,
-           code: this.props.match.params.room_id,
-         };
-    
-         axios
-           .post("/chat", chat)
-           .then((response) => {
-             if (response) {
-               this.setState({message: ''})
-             }
-           })
-           .catch((error) => {
-             console.log(error);
-           });
-       };
- }
+    this.handleChange = (event) => {
+      event.preventDefault();
+      this.setState({ message: event.target.value });
+    };
 
- componentDidMount(){
-  axios.get(`/chat/${this.props.match.params.room_id}`).then((response) => {
-          if (response.data.chats.length > 0) {
-            console.log(response.data.chats.length)
-             this.setState({ chats: response.data.chats })
-           }else{
-             this.setState({ chats: [{username: 'Chatty', message: 'Say hello!'}]})
-           }
-         });
- }
+    this.handleSubmit = (event) => {
+      event.preventDefault();
+      let newChat = {
+        username: this.props.match.params.username,
+        message: this.state.message,
+      };
+      this.socket.emit("message", newChat);
+      var chat = {
+        username: this.props.match.params.username,
+        message: this.state.message,
+        code: this.props.match.params.room_id,
+      };
+
+      axios
+        .post("/chat", chat)
+        .then((response) => {
+          if (response) {
+            this.setState({ message: "" });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`/chat/${this.props.match.params.room_id}`).then((response) => {
+      if (response.data.chats.length > 0) {
+        console.log(response.data.chats.length);
+        this.setState({ chats: response.data.chats });
+        this.scrollToBottom();
+      } else {
+        this.setState({
+          chats: [{ username: "Chatty", message: "Say hello!" }],
+        });
+      }
+    });
+  }
   render() {
     return (
       <div>
         <Nav />
         <Container>
-          <div className="chatbox">
-            <ChatContainer className="chat-container" messages={this.state.chats} />
+          <div className="chatbox" id="chatbox">
+            <ChatContainer
+              className="chat-container"
+              messages={this.state.chats}
+            />
           </div>
           <InputGroup>
             <FormControl
@@ -80,7 +95,11 @@ class Chat extends Component {
               onChange={(event) => this.handleChange(event)}
             />
             <InputGroup.Append>
-              <Button variant="primary" className="send" onClick={this.handleSubmit}>
+              <Button
+                variant="primary"
+                className="send"
+                onClick={this.handleSubmit}
+              >
                 Send
               </Button>
             </InputGroup.Append>
