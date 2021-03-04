0da28c044e78c897c48e00554750ee7d21b70539
Bradley Wilson <bluedirtbike6@gmail.com>
2021-03-04 15:58:27 -0600

    add mongodb uri, prepare for heroku deployment.

---
+++ client/build/precache-manifest.89181f3808ae63783f740d5bd9eea3fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0854d8bb22cdad4362f662c6f1bc7b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "215899a5a2dd2a35d554",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e5ba77e07e6da1c58251",
+    "url": "/static/css/main.c3ec66ee.chunk.css"
+  },
+  {
+    "revision": "215899a5a2dd2a35d554",
+    "url": "/static/js/2.fcc3eaf1.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.fcc3eaf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ba77e07e6da1c58251",
+    "url": "/static/js/main.9aac9aa4.chunk.js"
+  },
+  {
+    "revision": "7248a0cf8c6ccf85beb3",
+    "url": "/static/js/runtime-main.52454681.js"
+  }
+]);
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose")
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const path = require('path');
+require('dotenv').config()
 
 //port for environment
 var PORT = process.env.PORT || 3001;
@@ -14,8 +16,15 @@ app.use(express.json());
 //routes
 require("./routes/apiRoutes.js")(app);
 
-//making public a static folder
-app.use(express.static("public"));
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static("client/build"));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+    
+}
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ChattyReact";
 
